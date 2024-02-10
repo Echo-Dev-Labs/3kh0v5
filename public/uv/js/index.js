6574324c70e692c982afcf8e1011e9873ee822c6
xNC <133831293+NCCoder2@users.noreply.github.com>
2024-02-10 15:42:51 +0000

    Added function to the loading, redid styling for the loading page.

--- public/uv/js/index.js
+++ public/uv/js/index.js
@@ -18,5 +18,6 @@ form.addEventListener("submit", async (event) => {
   }
 
   const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  localStorage.setItem('url', __uv$config.prefix + __uv$config.encodeUrl(url));
+  location.href = "/load.html"
 });
