93bf3ada0cd953831d5ca3ef243019a555da3c6e
xNC <133831293+NCCoder2@users.noreply.github.com>
2024-02-16 18:41:39 +0000

    Games

--- public/games/games.js
+++ public/games/games.js
@@ -10,13 +10,17 @@ fetch('games.json')
         games.forEach(game => {
             const link = document.createElement('a');
             link.className = 'g';
-            link.href = gamecdn + game.root + "/" + game.file;
-
+            //link.href = gamecdn + game.root + "/" + game.file;
+            link.href = "/play/index.html";
             const img = document.createElement('img');
             img.src = gamecdn + game.root + "/" + game.img;
 
             const h3 = document.createElement('h3');
             h3.textContent = game.name;
+
+            link.addEventListener('click', () => {
+                localStorage.setItem('game', JSON.stringify(game));
+            });
 
             link.appendChild(img);
             link.appendChild(h3);
